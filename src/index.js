f5136be51d86013bd0eb80795092558ac883b572
CrisGO-Laptop <cristhiangiraldo053@gmail.com>
2023-01-18 10:36:58 -0500

    Se usó este commit para transferir los archivos de laptop a mac  ,codigo incompleto

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import {dirname, join} from 'path'
+import { fileURLToPath } from "url";
+
+const app = express();
+const PUERTO = process.env.PORT || 3000;
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.set('views', join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get
+
+app.listen(PUERTO);
+console.log(`El servidor esta escuchando en el puerto ${PUERTO}`);
